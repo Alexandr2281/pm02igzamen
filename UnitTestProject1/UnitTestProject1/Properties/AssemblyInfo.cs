cdf8f46aa270d528ba584316cad85399014c0838
Александр2281 <bebenkovalexander@yandex.ru>
2026-03-19 13:53:44 +0300

    тест

---
+++ UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cccf159-a2d5-41d8-88d5-2bc8af1f6763")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void TestDijkstraSimpleGraph()
+        {
+           
+            double[,] simpleGraph = new double[,]
+            {
+                {0, 1, 4},
+                {1, 0, 2},
+                {4, 2, 0}
+            };
+
+          
+            double[] shortestDistances = Graph.Dijkstra(simpleGraph, 0);
+
+            
+            double[] expectedDistances = new double[] { 0, 1, 3 };
+
+           
+            CollectionAssert.AreEqual(expectedDistances, shortestDistances);
+        }
+    }
+}
